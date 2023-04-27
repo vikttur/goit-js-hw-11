1e3082f6c24c682b1265f944963b16f9585b658d
Viktor Biruk <vikttur.job@gmail.com>
2023-04-27 12:40:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Notiflix from 'notiflix';
 
-// Notiflix.Notify.info('Too many matches found');
+// Notiflix.Notify.info('We're sorry, but you've reached the end of search results.');
 // Notiflix.Notify.failure('Oops, there is no country with that name');
+// Notiflix.Notify.info('Hooray! We found totalHits images.');
