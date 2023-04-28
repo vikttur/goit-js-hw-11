a03fabe7df825610200d3b6304c0e2c48016f0c6
Viktor Biruk <vikttur.job@gmail.com>
2023-04-28 18:37:38 +0300

    includes the axios library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import Notiflix from 'notiflix';
+import axios from 'axios';
+
+console.log(axios.isCancel('something')); // перевірка axios
+
+// axios.get('/user', {
+//     params: {
+//       ID: 12345
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
 
 // Notiflix.Notify.info('We're sorry, but you've reached the end of search results.');
 // Notiflix.Notify.failure('Oops, there is no country with that name');
